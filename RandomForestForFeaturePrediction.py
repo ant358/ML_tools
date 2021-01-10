8bd48be5201d038273c2848c0e8c65bda9543421
Anthony Wynne <61841178+AnthonyWynne@users.noreply.github.com>
2021-01-10 11:48:07 +0000

    Create RandomForestForFeaturePrediction.py

---
+++ RandomForestForFeaturePrediction.py
@@ -0,0 +1,54 @@
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
+import numpy as np 
+import matplotlib as plt
+
+def feature_importance(df, target):
+    """Genreric Random Forest For Feature Ranking
+    
+    Parameters
+    ----------
+    df : pandas dataframe
+    
+    target : str, the tilte of the target data column in the dataframe
+  
+    Returns
+    -------
+    None
+    Prints a dataframe of ranked features.
+    and
+    Prints a barchart of the feature importance
+    """
+
+    # create the target to predict
+    y_train = df[target]
+    X_train = df.drop([target], axis=1)
+    # Create a random forest regressor
+    clf = RandomForestRegressor(n_estimators=100, random_state=0, n_jobs=-1)
+
+    # Train the classifier
+    clf.fit(X_train, y_train)
+    
+    # collect the feature labels
+    feat_labels = list(X_train.columns)
+    feat_importance = list(clf.feature_importances_)
+    # get the standard deviation
+    std = list(np.std([tree.feature_importances_ for tree in clf.estimators_],
+                 axis=0))
+    # create the output table
+    features_df = pd.DataFrame({'features': feat_labels, 'importance': feat_importance, 'std': std} )
+    features_df.sort_values('importance', inplace=True, ascending=False)
+    print(features_df)
+    # create the output graph
+    plt.figure(figsize=(8,5))
+    plt.bar(x='features', height='importance',
+            data=features_df, yerr='std')
+    plt.ylim(-0.1, 0.5)
+    plt.ylabel('Metric Importance')
+    plt.grid(b=1, axis='y')
+    plt.title('The Importance of Features that effect the target')
+    plt.xticks(rotation=90)
+    plt.show()
+    
+    # unquote to return the df
+    # return features_df
