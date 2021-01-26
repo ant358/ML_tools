dfc446056c6f989469f1d051d12a8c3d613254f1
Anthony Wynne <61841178+AnthonyWynne@users.noreply.github.com>
2021-01-26 11:15:51 +0000

    Update RandomForestForFeaturePrediction.py

--- RandomForestForFeaturePrediction.py
+++ RandomForestForFeaturePrediction.py
@@ -1,10 +1,16 @@
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 import numpy as np 
 import matplotlib as plt
 
-def feature_importance(df, target):
-    """Genreric Random Forest For Feature Ranking
+def feature_importance_reg(df, target):
+    """Genreric Random Forest For Feature Ranking  
+    Designed for the early stages of model building 
+    so it takes the df with the target 
+    rather than an array
+    
+    Regression Version
     
     Parameters
     ----------
@@ -25,6 +31,61 @@ def feature_importance(df, target):
     X_train = df.drop([target], axis=1)
     # Create a random forest regressor
     clf = RandomForestRegressor(n_estimators=100, random_state=0, n_jobs=-1)
+
+    # Train the classifier
+    clf.fit(X_train, y_train)
+    
+    # collect the feature labels
+    feat_labels = list(X_train.columns)
+    feat_importance = list(clf.feature_importances_)
+    # get the standard deviation
+    std = list(np.std([tree.feature_importances_ for tree in clf.estimators_],
+                 axis=0))
+    # create the output table
+    features_df = pd.DataFrame({'features': feat_labels, 'importance': feat_importance, 'std': std} )
+    features_df.sort_values('importance', inplace=True, ascending=False)
+    print(features_df)
+    # create the output graph
+    plt.figure(figsize=(8,5))
+    plt.bar(x='features', height='importance',
+            data=features_df, yerr='std')
+    plt.ylim(-0.1, 0.5)
+    plt.ylabel('Metric Importance')
+    plt.grid(b=1, axis='y')
+    plt.title('The Importance of Features that effect the target')
+    plt.xticks(rotation=90)
+    plt.show()
+    
+    # unquote to return the df
+    # return features_df
+    
+def feature_importance_class(df, target):
+    """Genreric Random Forest For Feature Ranking  
+    Designed for the early stages of model building 
+    so it takes the df with the target 
+    rather than an array
+    
+    Classifier Version
+    
+    Parameters
+    ----------
+    df : pandas dataframe
+    
+    target : str, the tilte of the target data column in the dataframe
+  
+    Returns
+    -------
+    None
+    Prints a dataframe of ranked features.
+    and
+    Prints a barchart of the feature importance
+    """
+
+    # create the target to predict
+    y_train = df[target]
+    X_train = df.drop([target], axis=1)
+    # Create a random forest regressor
+    clf = RandomForestClassifier(n_estimators=100, random_state=0, n_jobs=-1)
 
     # Train the classifier
     clf.fit(X_train, y_train)
