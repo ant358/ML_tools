0e5c5f7dc6c5e672e1db483d77ac59df17e8d040
Anthony Wynne <61841178+AnthonyWynne@users.noreply.github.com>
2021-02-01 10:23:56 +0000

    updated get latest file

--- file_management.py
+++ file_management.py
@@ -1,10 +1,14 @@
 import os
+import glob
 
-def get_latest_file(extension='/*'):
+def get_latest_file(directory, extension='/*'):
     """Whats the most recent file in a directory
-    args: str (extension e.g. '/*.xlsx' provide the file extension defaults to all)
+    args: 
+        extension: str (extension e.g. '/*.xlsx' provide the file extension defaults to all)
+        directory: str (path to the directory needs to end in '\\')
+        
     return: str (path to the most recent file)
     """
-    list_of_files = glob.glob(bob_moves_path + extension) 
+    list_of_files = glob.glob(directory + extension) 
     latest_file = max(list_of_files, key=os.path.getctime)
     return os.path.join(latest_file)
